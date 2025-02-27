7e976065df7160d905f2f2530f7c3b2255cdfe76
Pankaj Maurya <100466858+Pankaj352@users.noreply.github.com>
2025-02-27 11:49:11 +0530

    Print developer by map function

--- script.js
+++ script.js
@@ -8,20 +8,55 @@ let arr = [
 
 function PrintDeveloperbyMap() {
   //Write your code here , just console.log
+   return arr.map((id) => {
+     if (id.profession === "developer") {
+       console.log(id.name);
+     }
+   });
 }
 
 function PrintDeveloperbyForEach() {
   //Write your code here , just console.log
+  arr.forEach((id) => {
+    if (id.profession === "developer") {
+      console.log(id.name);
+    }
+    });
 }
 
 function addData() {
   //Write your code here, just console.log
+  arr.push({
+    id: 4, name: "jane", age: "21",
+    profession: "developer"
+  });
+  console.log(arr);
 }
 
 function removeAdmin() {
   //Write your code here, just console.log
+  arr = arr.filter((id) => {
+    if (id.profession !== "admin") {
+      return id;
+    }
+  });
+  console.log(arr);
+  
 }
 
 function concatenateArray() {
   //Write your code here, just console.log
+  let arr2 = [
+    {
+      id: 5, name: "jane", age: "21", profession:
+        "developer"
+    },
+    {
+      id: 6, name: "john", age: "22", profession:
+        "admin"
+    }
+  ];
+  arr = arr.concat(arr2);
+  console.log(arr);
+  
 }
